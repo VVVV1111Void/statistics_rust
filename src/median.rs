87e8fd30432e6be0fafffdab6b1581d4a9afd3e0
void <infinitevoid424@protonmail.com>
2022-11-28 18:49:48 +0800

    separated occurance to its own function

--- src/median.rs
+++ src/median.rs
@@ -1,9 +1,8 @@
 pub fn median(array_sorted: &[i32]) -> f32 {
     let mid = array_sorted.len() / 2;
-    if array_sorted.len() % 2 == 0 { // even situation
+    if array_sorted.len() % 2 == 0 {
         (array_sorted[mid - 1] + array_sorted[mid]) as f32 / 2.0 // calculate mean whilst converting types
-    }
-    else {
+    } else {
         array_sorted[mid] as f32
     }
 }
--- src/mode.rs
+++ src/mode.rs
@@ -2,35 +2,36 @@ use std::cmp::Ordering;
 use std::collections::HashMap;
 use std::vec;
 
-pub fn mode(array: &[i32]) -> Vec<i32> {
+fn count_occurence(arr: &[i32]) -> HashMap<i32, i32> {
     let mut nums: HashMap<i32, i32> = HashMap::new();
-
-    for n in array {
+    for n in arr {
         let count = nums.entry(*n).or_insert(0);
         *count += 1;
-    } // Counts the occurence of each number in the vector/array
+    } // Counts the occurence of each number
+    nums
+}
 
+pub fn mode(array: &[i32]) -> Vec<i32> {
+    let nums = count_occurence(&array);
     let first = array.first();
     match first {
         None => {
-            vec![] // Empty vector
+            vec![]
         }
         Some(first) => {
-            let mut the_modes: Vec<i32> = vec![*first]; // Possible mode candidates alongside the current candidate
-            let mut prev_amt: i32 = nums.get(first).copied().unwrap(); // value of candidate
-                                                                       // Iterates over the nums comparing each one.
-                                                                       // Returns num with greatest value
+            let mut the_modes: Vec<i32> = vec![*first]; // possible candidates alongside
+            let mut prev_amt: i32 = nums.get(first).copied().unwrap(); // value
+
             for (k, pot_amt) in nums {
+                // iterates over nums
                 if k != the_modes[0] {
                     match prev_amt.cmp(&pot_amt) {
                         Ordering::Less => {
                             the_modes = vec![k]; // create a new vector of possible modes
-                            prev_amt = pot_amt
+                            prev_amt = pot_amt;
                         }
-
-                        Ordering::Greater => {}
-
-                        Ordering::Equal => the_modes.push(k), // appends the mode candidate
+                        Ordering::Greater => {}               // No action
+                        Ordering::Equal => the_modes.push(k), // Append candidate
                     }
                 }
             }
